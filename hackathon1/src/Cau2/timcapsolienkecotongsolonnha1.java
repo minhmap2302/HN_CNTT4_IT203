f8dc50f4895f5283b66bc417da9a707a41e79dba
Minh Béo <nguyentienminh.230206@gmail.com>
2026-02-25 11:03:57 +0700

    Add files via upload

--- hackathon1/src/Cau2/timcapsolienkecotongsolonnha1.java
+++ hackathon1/src/Cau2/timcapsolienkecotongsolonnha1.java
@@ -3,37 +3,31 @@ package Cau2;
 import java.util.Scanner;
 
 public class timcapsolienkecotongsolonnha1 {
-     static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[100];
-        System.out.print("Moi ban nhap mang");
-        int n = sc.nextInt();sc.nextLine();
-
-        if (n<=2){
+        System.out.print("Moi ban nhap so phan tu: ");
+        int n = sc.nextInt();
+        if (n < 2) {
             System.out.println("Khong hop le");
             return;
         }
-        for (int i = 0; i < n; i++){
-            System.out.print("Moi ban nhap chuoi: ");
-            arr[i] = sc.nextInt(); sc.nextLine();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Moi ban nhap so thu " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
         }
-        int max = arr[0];
-        int sum = arr[0];
+        int maxSum = arr[0] + arr[1];
         int n1 = arr[0];
-        int n2 = arr[0];
-        for (int i = 0; i < n; i++){
-            for (int j = i; j < n - i ; j++ ){
-                sum = arr[i] + arr[j];
-                if (sum>max){
-                    n1 = arr[i];
-                    n2 = arr[j];
-                    sum = max;
-                }
+        int n2 = arr[1];
+        for (int i = 1; i < n - 1; i++) {
+            int sum = arr[i] + arr[i + 1];
+            if (sum > maxSum) {
+                maxSum = sum;
+                n1 = arr[i];
+                n2 = arr[i + 1];
             }
-
         }
-        System.out.println("cap so lien ke co tong lon nhat la: " +n1+ "va" +n2+ "(Tong la: " +sum+ ")" );
-
+        System.out.println("Cap so lien ke co tong lon nhat la: " + n1 + " va " + n2+ " (Tong la: " + maxSum + ")");
     }
 }
 
