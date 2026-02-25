67f1a46a703412c6dc2da5b7c452766ca76fcdcb
Minh Béo <nguyentienminh.230206@gmail.com>
2026-02-25 11:05:37 +0700

    Add files via upload

--- hackathon1/src/Cau1/nhapvaochuoikitulahotendaydutubanphim2.java
+++ hackathon1/src/Cau1/nhapvaochuoikitulahotendaydutubanphim2.java
@@ -3,23 +3,20 @@ package Cau1;
 import java.util.Scanner;
 
 public class nhapvaochuoikitulahotendaydutubanphim2 {
-    public static String generateEmail(String fullName) {
-        fullName = fullName.trim().replaceAll("\\s+", " ");
-        fullName = fullName.toLowerCase();
-        String[] words = fullName.split(" ");
-        String lastName = words[words.length - 1];
-        StringBuilder initials = new StringBuilder();
-        for (int i = 0; i < words.length - 1; i++) {
-            initials.append(words[i].charAt(0));
-        }
-        return lastName + initials.toString() + "@rikkei.edu.com";
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhap ho ten: ");
-        String fullName = sc.nextLine();
-        String email = generateEmail(fullName);
+        String name = sc.nextLine();
+        name = name.trim().replaceAll("\\s+", " ");
+        name = name.toLowerCase();
+        String[] words = name.split(" ");
+        String ten = words[words.length - 1];
+        StringBuilder email = new StringBuilder();
+        email.append(ten);
+        for (int i = 0; i < words.length - 1; i++) {
+            email.append(words[i].charAt(0));
+        }
+        email.append("@rikkei.edu.com");
         System.out.println("Email: " + email);
     }
 }
